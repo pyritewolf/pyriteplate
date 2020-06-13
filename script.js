932256be7c73e7f0d6310ce34e4fae6c7fc4cb3d
Jules Morelli <jmorelli@manas.tech>
2020-06-13 18:13:57 -0300

    Add navigation to the side of the site

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const copyCSS = async (button, fileName) => {
     button.innerHTML = "Copy CSS";
   }, 2000);
 }
+
+// generate navigation dynamically because I'm a lazy ass
+window.onload = () => {
+  const main = document.getElementById('main');
+  const nav = document.getElementById('nav');
+  const originalTitles = main.getElementsByTagName("h2") || [];
+  const titles = new Set();
+  for (let element in originalTitles) {
+    const title = originalTitles.item(element)
+    titles.add(title);
+  }
+  titles.forEach(title => {
+    let titleText = title.textContent.replace(" Copy CSS", "").trim();
+    let titleSlug = encodeURI(titleText.replace(/ /g, "-").toLowerCase());
+    let localLink = document.createElement("a");
+    localLink.setAttribute("name", titleSlug);
+    main.insertBefore(localLink, title)
+    let navigationLink = document.createElement("a");
+    navigationLink.setAttribute("href", `#${titleSlug}`);
+    navigationLink.textContent = titleText;
+    nav.appendChild(navigationLink);
+  })
+}
