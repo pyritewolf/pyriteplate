703fc987473437d5c44b2bfbf0cd591539b3c900
Jules Morelli <jmorelli@manas.tech>
2020-06-14 12:28:17 -0300

    Add scrollspy to sidebar

--- script.js
+++ script.js
@@ -8,6 +8,39 @@ const copyCSS = async (button, fileName) => {
   }, 2000);
 }
 
+let currentScrolling;
+const isAtBottom = () => (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight
+const scrollTo = (to, duration) => () => {
+  var difference = to.offsetTop - window.pageYOffset;
+  var perTick = difference / duration * 10;
+
+  if (currentScrolling !== undefined) clearTimeout(currentScrolling)
+  currentScrolling = setTimeout(() => {
+    window.scroll(0, window.pageYOffset + perTick);
+    if (window.pageYOffset === to.offsetTop || (isAtBottom() && isScrollDown(window.pageYOffset))) return;
+    scrollTo(to, duration - 10)();
+  }, 10);
+}
+
+
+// making a cute scrollspy for the navbar
+let currentlyActiveIndex;
+let lastOffset = 0;
+const isScrollDown = (newOffset) => {
+  let retValue = false;
+  if (newOffset > lastOffset) retValue = true;
+  lastOffset = newOffset;
+  return retValue;
+}
+const setSelectedNavLink = (index, removalIndex) => {
+  if (!isNaN(index))
+    anchors[index].element.setAttribute('data-selected', 'true')
+  if (!isNaN(removalIndex))
+    anchors[removalIndex].element.removeAttribute('data-selected')
+  currentlyActiveIndex = index;
+}
+
+const anchors = [];
 // generate navigation dynamically because I'm a lazy ass
 window.onload = () => {
   const main = document.getElementById('main');
@@ -21,12 +54,30 @@ window.onload = () => {
   titles.forEach(title => {
     let titleText = title.textContent.replace(" Copy CSS", "").trim();
     let titleSlug = encodeURI(titleText.replace(/ /g, "-").toLowerCase());
-    let localLink = document.createElement("a");
-    localLink.setAttribute("name", titleSlug);
-    main.insertBefore(localLink, title)
+    let anchor = document.createElement("a");
+    anchor.setAttribute("name", titleSlug);
+    main.insertBefore(anchor, title)
     let navigationLink = document.createElement("a");
-    navigationLink.setAttribute("href", `#${titleSlug}`);
+    navigationLink.onclick = scrollTo(anchor, 250);
     navigationLink.textContent = titleText;
+    anchors.push({position: anchor.offsetTop, element: navigationLink});
     nav.appendChild(navigationLink);
-  })
+  });
+  anchors.sort((a, b) => a.position - b.position);
+
+  window.onscroll = () => {
+    let lookupIndex = 0;
+    currentOffset = window.pageYOffset;
+    if (isScrollDown(currentOffset)) {
+      if (!isNaN(currentlyActiveIndex)) lookupIndex = currentlyActiveIndex + 1;
+      if (!anchors[lookupIndex] || currentOffset >= anchors[lookupIndex].position)
+        setSelectedNavLink(lookupIndex, currentlyActiveIndex);
+    }
+    else {
+      if (currentlyActiveIndex !== 0) lookupIndex = currentlyActiveIndex - 1;
+      else lookupIndex = undefined;
+      if (!anchors[currentlyActiveIndex] || currentOffset < anchors[currentlyActiveIndex].position)
+        setSelectedNavLink(lookupIndex, currentlyActiveIndex);
+    }
+  }
 }
